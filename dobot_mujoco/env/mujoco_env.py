4ec1ae73508f71546193eed0d1d98e0302ac98c7
Kohio DEFLESSELLE <kohiodef@gmail.com>
2025-10-17 09:55:16 +0200

    WIP

--- dobot_mujoco/env/mujoco_env.py
+++ dobot_mujoco/env/mujoco_env.py
@@ -1,1 +1,16 @@
 # TODO: Implement the mujoco environment for the Dobot robotic arm
+from abc import ABC, abstractmethod
+from typing import Any
+
+import numpy as np
+import gymnasium as gym
+from gymnasium_robotics.envs.robot_env import MujocoRobotEnv
+from gymnasium import spaces
+
+
+class DobotBaseEnv(MujocoRobotEnv):
+    pass
+
+
+if __name__ == "__main__":
+    env = DobotBaseEnv()  
